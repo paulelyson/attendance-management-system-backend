2fec49d4e6019968e3bbaa0d0304c5bf16f17354
paulelyson <paulelyson.villaceran@gmail.com>
2025-09-25 18:20:51 +0800

    updated user create and get

--- models/User.ts
+++ models/User.ts
@@ -4,12 +4,17 @@ interface UserInterface {
   firstName: string;
   lastName: string;
   email: string;
+  dis: boolean;
 }
 
-const UserSchema = new Schema<UserInterface>({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-});
+const UserSchema = new Schema<UserInterface>(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    dis: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('user', UserSchema)
+export default mongoose.model('user', UserSchema);
--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,22 +1,27 @@
 import { Router, Request, Response } from 'express';
+import User from './../models/User';
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) =>
   Promise.resolve()
-    .then(() => {
-      res.json({ data: null, message: 'success get', success: true });
+    .then(async () => {
+      let users = await User.find({}).lean()
+      res.json({ data: users, message: 'Success getting users', success: true });
     })
     .catch((err: Error) => {
       res.json({ data: null, message: err.message, success: true });
     })
 );
 
-
 router.post('/', async (req: Request, res: Response) =>
   Promise.resolve()
-    .then(() => {
-      res.json({ data: null, message: 'success get', success: true });
+    .then(async () => {
+      let payload = req.body;
+      return await User.create(payload);
+    })
+    .then((resp) => {
+      res.json({ data: resp, message: 'Success creating user', success: true });
     })
     .catch((err: Error) => {
       res.json({ data: null, message: err.message, success: true });
